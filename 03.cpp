b8fa8786e3c40f2a8405575a3dbb643eadfce582
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-07 12:49:35 +0600

    added

--- 03.cpp
+++ 03.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    float a;
+    float b;
+    float sum;
+    cout << "Enter First Number: ";
+    cin >> a;
+    cout << "Enter Second Number: ";
+    cin >> b;
+    
+    sum = a + b;
+
+    cout << "The Sum is: " << sum << endl;
+    return 0;
+}

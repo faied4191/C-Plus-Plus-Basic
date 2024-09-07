5bbf21799cf71e4e9d438951cc00db1154f8fda9
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-07 12:47:12 +0600

    added

--- 02.cpp
+++ 02.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string a;
+    string b;
+    cout << "Enter Your Name: ";
+    getline(cin, a);
+    cout << "Enter Your ID: ";
+    getline(cin, b);
+    
+    cout << "Name: " << a << endl;
+    cout << "ID: " << b << endl;
+    cout << "Length of Name= " << a.length();
+    return 0;
+}

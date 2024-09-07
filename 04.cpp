0b33e2010bc63278f7023f9c868296acf11811b9
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-07 12:51:18 +0600

    added

--- 04.cpp
+++ 04.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter Hour of the Day: ";
+    cin >> a;
+
+    if (a < 10) {
+        cout << "Good morning";
+    } else if (a < 20) {
+        cout << "Good day";
+    } else {
+        cout << "Good evening";
+    }
+    return 0;
+}

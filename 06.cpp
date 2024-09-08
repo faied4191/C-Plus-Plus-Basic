bedddb34d16c058dea7899d856a734b47ea9c332
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-09 05:29:27 +0600

    added

--- 06.cpp
+++ 06.cpp
@@ -6,3 +6,29 @@ int main() {
     int a;
     cout << "Enter Day Number of the Week: ";
     cin >> a;
+
+    switch (a) {
+    case 1:
+        cout << "Monday";
+        break;
+    case 2:
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "Wednesday";
+        break;
+    case 4:
+        cout << "Thursday";
+        break;
+    case 5:
+        cout << "Friday";
+        break;
+    case 6:
+        cout << "Saturday";
+        break;
+    case 7:
+        cout << "Sunday";
+        break;
+    }
+    return 0;
+}

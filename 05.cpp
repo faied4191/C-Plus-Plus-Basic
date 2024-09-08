09b6a5c592490a41087c21a2a3a41f0b27ec01d9
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-09 05:25:27 +0600

    added

--- 05.cpp
+++ 05.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int c;
+    int greatest;
+    cout << "Enter First Number: ";
+    cin >> a;
+    cout << "Enter Second Number: ";
+    cin >> b;
+    cout << "Enter Third Number: ";
+    cin >> c;

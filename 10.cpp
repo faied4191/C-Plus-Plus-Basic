40e7ea880003ec600124c56baee0036d2299d125
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-11 08:52:48 +0600

    added

--- 10.cpp
+++ 10.cpp
@@ -5,3 +5,13 @@ using namespace std;
 float isSum(float x, float y) {
   return x + y;
 }
+
+float isMax(float x, float y) {
+  float large;
+  if (x > y) {
+    large = x;
+  } else {
+    large = y;
+  }
+  return large;
+}

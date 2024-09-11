85977c5da499e093316aaf352733ab874c0d79eb
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-11 08:49:57 +0600

    added

--- 09.cpp
+++ 09.cpp
@@ -5,3 +5,11 @@ using namespace std;
 float isSum(float x, float y) {
     return x + y;
 }
+
+int main() {
+    float a;
+    float b;
+    cout << "Enter the first number: ";
+    cin >> a;
+    cout << "Enter the second number: ";
+    cin >> b;

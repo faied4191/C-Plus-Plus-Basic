f1eb0239bda0cc00973e6308e485407222fdf494
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-14 17:38:40 +0600

    added

--- 12.cpp
+++ 12.cpp
@@ -7,3 +7,14 @@ int main() {
     cout << "Enter the number of elements: ";
     cin >> a;
     string arr1[a];
+
+    for (int i = 0; i < a; i++) {
+        cout << "Enter element names " << i + 1 << ": ";
+        cin >> arr1[i];
+    }
+
+    cout << "First Element Name: " << arr1[0] << endl;
+    cout << "First Element Size: " << sizeof(arr1[0]) << endl;
+    cout << "Array Size: " << sizeof(arr1) / sizeof(arr1[0]) << endl;
+    return 0;
+}

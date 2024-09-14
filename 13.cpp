f6c3318010926bfbc759f018acf59b2ec7fa61aa
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-14 17:41:23 +0600

    added

--- 13.cpp
+++ 13.cpp
@@ -12,3 +12,11 @@ int main() {
     cout << "Enter element names " << i + 1 << ": ";
     cin >> arr1[i];
  }
+
+ cout << " " << endl;
+ cout << "Elements and values: " << endl;
+ for (int i = 0; i < a; i++) {
+    cout << "arr1[" << i << "] = " << arr1[i] << endl;
+ }
+ return 0;
+}

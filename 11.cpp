98c16021c3f55fb5b68c1128939f2bf28ee28c04
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-14 17:36:24 +0600

    added

--- 11.cpp
+++ 11.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string arr1[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+    arr1[0] = "Opel";
+    cout << "First Element Name: " << arr1[0] << endl;
+    cout << "First Element Size: " << sizeof(arr1[0]) << endl;
+    cout << "Array Size: " << sizeof(arr1) / sizeof(arr1[0]) << endl;
+    return 0;
+}

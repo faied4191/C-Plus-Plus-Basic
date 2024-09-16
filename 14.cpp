4469376540e9993f1164577ad3247b4d327ca70c
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-16 16:34:32 +0600

    added

--- 14.cpp
+++ 14.cpp
@@ -7,3 +7,16 @@ public:
     string title;
     string author;
     int year;
+
+    Book(string title, string author, int year) {
+        this->title = title;
+        this->author = author;
+        this->year = year;
+    }
+
+    void display() {
+        cout << "Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "Year: " << year << endl;
+    }
+};

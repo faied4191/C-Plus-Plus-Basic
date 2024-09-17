507a2fd2cc8cabdea05a8183e21b1e42fc45e93c
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-18 00:09:30 +0600

    added

--- 18.cpp
+++ 18.cpp
@@ -12,3 +12,11 @@ using namespace std;
   void add(int a, int b, int c) {
     cout << "The Sum is: " << a + b + c << endl;
   }
+
+int main() {
+
+  add(5, 3);
+  add(2.5, 1.8);
+  add(1, 2, 3);
+  return 0;
+}
